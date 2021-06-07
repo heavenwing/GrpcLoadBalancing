78d61b747c715c75a6a0908ca520ea515f6c9459
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2021-06-07 12:04:35 +0800

    add missing file

--- x509/gen.sh
+++ x509/gen.sh
@@ -9,7 +9,7 @@ openssl x509 -in ca-cert.pem -noout -text
 
 # 2. Generate web server's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem \
-    -subj "/C=CN/ST=Sichuan/L=Chengdu/O=ZYG/OU=grpc/CN=grpc.zyg.cloud/emailAddress=heavenwing@msn.com"
+    -subj "/C=CN/ST=Sichuan/L=Chengdu/O=ZYG/OU=GRPC/CN=grpc.zyg.cloud/emailAddress=heavenwing@msn.com"
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
